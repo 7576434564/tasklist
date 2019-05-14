3f84d16bd6ca70301585256d71748f091b4a10bb
junya.matsumoto <ybkmkt.mj.0113@gmail.com>
2019-05-14 14:40:37 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -22,11 +22,10 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+      
     if @task.update(task_params)
       redirect_to @task
     else
@@ -35,7 +34,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
+      
     @task.destroy
     
     redirect_to tasks_url
@@ -43,6 +42,10 @@ class TasksController < ApplicationController
   
   private
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
